7fb5ee437a1718b5b168bddb55ceaa2e52b624be
ThomasGabriel2 <91309110+ThomasGabriel2@users.noreply.github.com>
2023-12-08 11:31:26 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ my-app/src/components/Login/Login.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Form, Header,Button } from 'semantic-ui-react'
+
+export const Login = () =>{
+    const [currentUser,setCurrentUser]= useState({
+        login:"",
+        pwd:"",
+    });
+
+    function submitOrder(data){
+        console.log(data)
+    }
+
+    function processInput(event, { valueData }){
+        const target = event.currentTarget;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        setCurrentUser({...currentUser, [name]: value});
+    }
+
+
+    return (
+        <Form>
+            <Header as='h4' dividing>
+                User Registration
+            </Header>
+            <Form.Field>
+                <Form.Input label="Login" placeholder="Login" onChange={processInput} name="login" value={currentUser.login}/>
+            </Form.Field>
+            <Form.Field>
+                <Form.Input type="password" label="Pwd"  placeholder="" onChange={processInput} name="pwd" value={currentUser.pwd}/>
+            </Form.Field>
+            <Button type='submit' onClick={submitOrder}>Submit</Button>
+        </Form>
+
+    );
+
+}
