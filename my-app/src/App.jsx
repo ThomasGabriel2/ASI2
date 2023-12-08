e99a677ee2ae867c56ddfa28570f39c6fb662947
ThomasGabriel2 <91309110+ThomasGabriel2@users.noreply.github.com>
2023-12-08 11:31:26 +0100

    refactor Store

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Store from './components/Store.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Login} from './components/Login/Login.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Store></Store>
+      <>
+          <BrowserRouter>
+              <div>
+                  <Routes>
+                      <Route path='/login' element={<Login/>} />
+                  </Routes>
+              </div>
+          </BrowserRouter>
+      </>
+
   )
 }
 
