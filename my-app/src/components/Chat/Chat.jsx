ef9a4f227008c1002d85e63c061fb090cde5a921
ThomasGabriel2 <91309110+ThomasGabriel2@users.noreply.github.com>
2023-12-20 17:10:16 +0100

    add reception Message

--- my-app/src/components/Chat/Chat.jsx
+++ my-app/src/components/Chat/Chat.jsx
@@ -9,49 +9,53 @@ export const Chat = () => {
     let user = useSelector(state => state.userReducer.authUser);
     const [usersConnected, setUsersConnected] = useState([])
     const [messages, setMessages] = useState([])
+    const [socket, setSocket] = useState(undefined)
+    const [chatUser, setChatUser] = useState(undefined)
     const receiveMessage = (msg) => {
-        setMessages( messages + msg.mess)
+        setMessages( previous => [...previous,  msg.mess])
     }
 
     const receiveUsers = (users) => {
         setUsersConnected(users)
-        console.log(users)
 
     }
 
-    const URL = 'http://localhost:8011/';
-    const socket = io(URL, {
-        query: {
-            id: user.id
-        }
-    }, );
-    console.log(socket)
-    socket.on('receive message', receiveMessage)
-    socket.on('reception users', receiveUsers)
+    const changeChatUser= () => {
+        setMessages([])
+        setChatUser(user)
+    }
 
+    useEffect(()=>{
+        const URL = 'http://localhost:8011/';
+        const socketTmp = io(URL, {
+            query: {
+                id: user.id
+            }
+        }, );
+        socketTmp.on('receive message', receiveMessage);
+        socketTmp.on('reception users', receiveUsers);
+        setSocket(socketTmp)
+    },[]);
 
 
     const refreshUsers = () => {
         console.log(socket)
         socket.emit('refresh users');
     }
-
-    refreshUsers()
     let display_messages = messages.map((message) => <div>{message}</div>)
-    let display_users = usersConnected.map((user) => <Menu.Item key={user.id}>user.surName</Menu.Item>)
+    let display_users = usersConnected.map((user) => <Menu.Item onClick={changeChatUser(user)} key={user.id}>{user.surName}</Menu.Item>)
 
     return (
     <div>
         <p>Votre ID unique est : <span id="uniqueId">{user.id}</span></p>
-        <Menu vertical fixed="right">
+        <Menu   >
             <Menu.Item header>Users</Menu.Item>
             {display_users}
-            <Menu.Item position={bottom}>
-                <Button type='submit' onClick={refreshUsers}>Submit</Button>
+            <Menu.Item position="right">
+                <Button type='submit' onClick={refreshUsers}>Refresh</Button>
             </Menu.Item>
-
         </Menu>
-
+        
         {display_messages}
     </div>
     )
