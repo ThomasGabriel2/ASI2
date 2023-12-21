890c1af47d15de537af8eb2631c37dcfee9afd29
Maxime CHOURAQUI <72150765+Emmixe@users.noreply.github.com>
2023-12-21 12:01:38 +0100

    Add files via upload

--- tp/app/models/User.js
+++ tp/app/models/User.js
@@ -1,9 +1,10 @@
 class User {
-    constructor({ id, lastName, surName, pwd}) {
+    constructor({ id, lastName, surName, pwd, cardList}) {
         this.id = id;
         this.lastName = lastName;
         this.surName = surName;
         this.pwd = pwd;
+        this.cardList = cardList;
     }
 }
 
